6a33e3d368b1ca30ec6de257105674e37c40947b
BadrMorad <moradpro989@gmail.com>
2024-10-11 14:11:57 +0200

    exo1.php

---
+++ exo1.php
@@ -0,0 +1,14 @@
+<h1>Exercice1</h1>
+
+<p>Soit la « Noter formation DL commerce aujourd'hui ».</p>
+
+
+
+<h2>Résultat</h2>
+
+<?php
+
+$phrase = "Noter formation DL commerce aujourd'hui";
+$longueur = strlen($phrase);
+echo "La phrase «$phrase » contient $longueur caractéres<br>";
+echo "La phrase «$phrase » contient ". strlen($phrase)." caractéres<br>";
