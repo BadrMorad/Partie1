f853b7c48866e4aca48ca7936b956a7783ed24ec
BadrMorad <moradpro989@gmail.com>
2024-10-18 09:11:46 +0200

    php

--- exo5.php
+++ exo5.php
@@ -8,9 +8,6 @@ $montant_francs = 200;
 $taux = 15.24/100;
 $montant_euros = $montant_francs * $taux;
 
-
-
-
 echo"Montant en francs = " . $montant_francs . "<br>";
 echo $montant_francs . "francs = " . $montant_euros . " €";
 ?>
